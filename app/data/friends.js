fc0cab410cac1ea80128e4506dcc573e3475cf2f
Louis Rollins <louisrollins01@gmail.com>
2019-07-23 18:32:39 -0400

    answers push to /api/friends

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+    {
+        name: 'Aragorn',
+        photo: 'https://vignette.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423',
+        scores: [4, 4, 4, 5, 2, 2, 4, 5, 5, 1]
+    },
+    {
+        name: 'Gandalf',
+        photo: 'https://vignette.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754',
+        scores: [5, 2, 5, 5, 3, 1, 3, 2, 5, 5]
+    },
+    {
+        name: 'The Witch King of Angmar',
+        photo: 'https://www.htxt.co.za/wp-content/uploads/2016/11/Witch-king-of-Angmar-Lord-of-the-Rings-3D-Printed-Header-Image-htxt.africa.png',
+        scores: [1, 5, 5, 1, 5, 5, 3, 5, 1, 3]
+    },
+    {
+        name: 'Gollum',
+        photo: 'https://i.ytimg.com/vi/rvP3dlIbwBU/maxresdefault.jpg',
+        scores: [4, 2, 1, 1, 1, 3, 2, 1, 1, 2]
+    },
+    {
+        name: 'Legolas',
+        photo: 'https://vignette.wikia.nocookie.net/lotr/images/3/33/Legolas_-_in_Two_Towers.PNG/revision/latest?cb=20120916035151',
+        scores: [4, 5, 5, 5, 4, 3, 4, 5, 2, 4]
+    },
+    {
+        name: 'Pippin',
+        photo: 'https://vignette.wikia.nocookie.net/lotr/images/0/0a/Pippinprintscreen.jpg/revision/latest?cb=20060310083048',
+        scores: [3, 2, 3, 3, 3, 2, 4, 2, 4, 5]
+    }
+]
+module.exports = friends
